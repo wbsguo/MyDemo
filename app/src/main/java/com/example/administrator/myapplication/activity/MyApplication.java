3ce4aee4560074e160c39d1fafaf1dbcafbddbf2
wangbs <563492052@qq.com>
2016-05-13 17:50:25 +0800

    地方

---
+++ app/src/main/java/com/example/administrator/myapplication/activity/MyApplication.java
@@ -0,0 +1,13 @@
+package com.example.administrator.myapplication.activity;
+
+import android.app.Application;
+
+/**
+ * Created by wangbs on 16/5/13.
+ */
+public class MyApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
